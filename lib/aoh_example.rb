52f4482d2edb5f892fc4ef3cce94fb771761cdb6
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-11-01 18:23:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -36,6 +36,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
+  result[2][:name] = "George Orwell"
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
