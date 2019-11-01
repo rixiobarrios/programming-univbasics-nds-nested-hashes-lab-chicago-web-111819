7bc7cdac0d66c1fbd9ab2cef5ce5b293994b6d09
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-11-01 18:28:41 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -38,6 +38,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   aoh[row][key] = new_array
+  return aoh
   #nested_array[2][:name] = "George Orwell"
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
